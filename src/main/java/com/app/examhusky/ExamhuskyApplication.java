dd215072c6da084a631d0280030c0a1e9ca22782
Farhat Shahir Zim <cybertronix.4406@gmail.com>
2023-12-18 17:11:54 +0600

    WIP cli command runner for seeder

--- src/main/java/com/app/examhusky/ExamhuskyApplication.java
+++ src/main/java/com/app/examhusky/ExamhuskyApplication.java
@@ -1,13 +1,19 @@
 package com.app.examhusky;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExamhuskyApplication {
+public class ExamhuskyApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamhuskyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Add seeder here!");
+	}
 }
